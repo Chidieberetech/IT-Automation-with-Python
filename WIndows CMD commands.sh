b608f5d2fe59a3387145d05054cd818c1430d309
Teroxlab <emmanuels820@gmail.com>
2022-10-03 01:22:07 +0100

    The Windows command prompt

---
+++ WIndows CMD commands.sh
@@ -0,0 +1,17 @@
+    1. ASSOC: Fix File Associations
+    2. FC: File Compare
+    3. IPCONFIG: IP Configuration
+    4. NETSTAT: Network Statistics
+    5. PING: Send Test Packets
+    6. TRACERT: Trace Route
+    7. POWERCFG: Power Configuration
+    8. SHUTDOWN: Turn Off Computer
+    9. SYSTEMINFO: System Information
+    10. SFC: System File Checker
+    11. NET USE: Map drives
+    12. CHKDSK: Check Disk
+    13. SCHTASKS: Schedule Tasks
+    14. ATTRIB: Change File Attributes
+    Other Windows CMD Commands
+
+    
