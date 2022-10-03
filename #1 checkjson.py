ae677d3f0fec4b1bf7320fce0688a3ade2148bd4
Teroxlab <emmanuels820@gmail.com>
2022-10-03 22:09:56 +0100

    This script will read a file and either pass the file

--- #1 checkjson.py
+++ #1 checkjson.py
@@ -1,4 +1,4 @@
-#This script will read a file and either pass the file 
+#This script will read a file and either pass the file
 # as being a valid JSON file, or die a horrible death. But for all practical reasons, 
 # it tells me if the error is in the file or the program that I am trying to load the file into.
 import os
@@ -18,5 +18,6 @@ else:
 
 #To run it:
 
-#   $ checkjson test.json
+#....>   $ checkjson test.json
+
 #That’s all you need to validate JSON, but it can easily be modified to validate YAML, as well.
