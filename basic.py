f6988ddbd37cf6f3636de1950e5462b4cd1362ed
Teroxlab <emmanuels820@gmail.com>
2022-11-08 19:39:44 +0100

    programmed a script in Python

---
+++ basic.py
@@ -0,0 +1,9 @@
+print("I'm programming in Python!")
+#Here is your output:
+#I'm programming in Python!
+
+#Nice job! You just programmed a script in Python that says
+#that you are programming in Python, which is pretty meta.
+#It also means you just wrote your first Python script and
+#got your computer to do what you want.  Feels pretty great,
+#right?
