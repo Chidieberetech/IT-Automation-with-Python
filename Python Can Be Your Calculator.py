c18496972e1a6a6b113e8fac1c4fe5ef0bdd8288
Teroxlab <emmanuels820@gmail.com>
2022-11-08 20:06:16 +0100

    Basic Math Operators.py

---
+++ Python Can Be Your Calculator.py
@@ -0,0 +1,17 @@
+# Simple_Division,Subtraction,Addition,Multiply Python
+# function definition
+def divide(a, b):
+    return a / b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
